cdb4a6125c221558e64d685eab8661231bdc8c23
Samarth Sharma <samarth@Samarths-MacBook-Air.local>
2025-08-15 20:50:26 +0530

    Vercel

--- server/index.js
+++ server/index.js
@@ -89,10 +89,4 @@ app.use('*', (req, res) => {
     });
 });
 
-const PORT = process.env.PORT || 5001;
-
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
-
 module.exports = app;
