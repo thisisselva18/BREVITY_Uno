118442585a1bf811614da57826a82c6ea58ebe92
saysamarth <125565160+saysamarth@users.noreply.github.com>
2025-08-17 11:16:00 +0530

    Revert "Backend: Implement User Reactions Feature with Persistence and Duplicate Prevention"

--- server/routes/reaction.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { toggleReaction } = require('../controllers/reaction');
-const { authMiddleware } = require('../middleware/auth');
-
-const router = express.Router();
-
-router.post('/',authMiddleware ,toggleReaction);
-
-module.exports = router;
